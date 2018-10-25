81395ea6fd1a0c3101b23c55d74854a225491a2d
Henry Yang <yangh6@rose-hulman.edu>
2018-10-25 16:17:06 -0400

    renamed

--- src/m1_Hanyu Yang.py
+++ src/m1_Hanyu Yang.py
@@ -27,8 +27,6 @@ def run_test_color():
     while True:
         robot.drive_system.start_moving(20, 20)
         if robot.color_sensor.get_color() != rb.Color.BLACK.value:
-
-
-
+            robot.drive_system.turn_degrees(3)
 
 main()

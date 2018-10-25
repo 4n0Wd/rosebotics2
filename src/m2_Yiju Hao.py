8c4458e9064d3bd5d298956330ec74cd77e5cf4a
haoy1 <haoy1@rose-hulman.edu>
2018-10-25 16:54:18 -0400

    .

--- src/m2_Yiju Hao.py
+++ src/m2_Yiju Hao.py
@@ -10,7 +10,7 @@ import time
 def main():
     """ Runs YOUR specific part of the project """
 
-    #run_test_touch_sensor()
+    # run_test_touch_sensor()
     run_test_touch_sensor_10()
 
 def run_test_touch_sensor():
@@ -18,14 +18,11 @@ def run_test_touch_sensor():
     robot = rb.Snatch3rRobot()
 
     print()
-    print("Testing the  touch_sensor  of the robot.")
-    print("Repeatedly press and release the touch sensor.")
-    print("Press Control-C when you are ready to stop testing.")
     time.sleep(1)
     count = 1
     while True:
-        print("{:4}.".format(count),
-              "Touch sensor value is: ", robot.touch_sensor.get_value())
+        print(count,
+              ".Touch sensor value is: ", robot.touch_sensor.get_value())
         time.sleep(0.5)
         count = count + 1
 
